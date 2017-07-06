4e9edee9c5179cb4cc0fd7531d471d889a42a109
miyamoto.atsushi <miyamoto.atsushi@tis.co.jp>
2017-07-06 19:45:54 +0900

    e

--- attendance/results.py
+++ attendance/results.py
@@ -5,7 +5,10 @@ from util import db_connection
 
 def check_result(db_conn, request_json):
     sql = "select attendance_date, status from results where user_id = (%s) and attendance_date = to_date((%s), 'yyyy/mm/dd') "
-    param = [request_json["user_id"], request_json["attendance_date"]]
+    param = [
+        request_json["user_id"],
+        request_json["attendance_date"]
+    ]
     results = db_conn.select_dict(sql, param)
 
     if results and len(results) > 0:
@@ -17,9 +20,12 @@ def check_result(db_conn, request_json):
     return "未登録"
 
 
-def insert(db_conn, request_json):
-    sql = "insert into () attendance_date, status from results where user_id = (%s) and attendance_date = to_date((%s)) "
-    param = [request_json["user_id"], request_json["attendance_date"]]
+def calc_time(db_conn, request_json):
+    sql = "select attendance_date, status from results where user_id = (%s) and attendance_date = to_date((%s), 'yyyy/mm/dd') "
+    param = [
+        request_json["user_id"],
+        request_json["attendance_date"]
+    ]
     results = db_conn.select_dict(sql, param)
 
     if results and len(results) > 0:
@@ -29,6 +35,42 @@ def insert(db_conn, request_json):
             return "承認待ち"
 
     return "未登録"
+
+
+def insert_work(db_conn, request_json):
+    sql = "insert into results values((%s), to_date((%s), 'yyyy/mm/dd'), '1', '1', (%s), to_time((%s), 'yyyy/mm/dd hh24:mi:ss'), to_time((%s), 'yyyy/mm/dd hh24:mi:ss'), to_time((%s), 'hh24:mi:ss'), to_time((%s), 'hh24:mi:ss'), (%s), null, null, (%s), null) "
+    param = [
+        request_json["user_id"],
+        request_json["attendance_date"],
+        request_json["work_division"],
+        request_json["start_time"],
+        request_json["end_time"],
+        request_json["work_time"],
+        request_json["rest_time"],
+        request_json["delay_reason"],
+        request_json["remarks"]
+    ]
+    db_conn.update(sql, param)
+
+    return "OK"
+
+
+def insert_holiday(db_conn, request_json):
+    sql = "insert into results values((%s), to_date((%s), 'yyyy/mm/dd'), '1', '2', null, null, null, null, null, null, (%s), (%s), (%s), null) "
+    param = [
+        request_json["user_id"],
+        request_json["attendance_date"],
+        request_json["work_division"],
+        request_json["start_time"],
+        request_json["end_time"],
+        request_json["work_time"],
+        request_json["rest_time"],
+        request_json["delay_reason"],
+        request_json["remarks"]
+    ]
+    db_conn.update(sql, param)
+
+    return "OK"
 
 
 if __name__ == "__main__":
---
+++ attendance/time_master.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import datetime
+from util import db_connection
+
+
+def get_rest_time(db_conn, request_json):
+    sql = "select start_time, end_time from time_master where work_division = (%s) and time_division = '2' "
+    param = [request_json["work_division"]]
+    results = db_conn.select_dict(sql, param)
+
+    return get_minute_work_rest_time(
+        request_json["start_time"],
+        request_json["end_time"],
+        results
+    )
+
+
+def get_minute_work_rest_time(str_start_time, str_end_time, rest_time_list):
+
+    dt_start_time = datetime.datetime.strptime(str_start_time.split(" ")[1], "%H:%M:%S")
+    dt_end_time = datetime.datetime.strptime(str_end_time.split(" ")[1], "%H:%M:%S")
+    if dt_start_time >= dt_end_time:
+        dt_end_time += datetime.timedelta(days=1)
+    diff_time = dt_end_time - dt_start_time
+
+    total_rest_time_minute = 0
+    for i, rest_time in enumerate(rest_time_list):
+        rest_start_time = datetime.datetime.strptime(rest_time["start_time"], "%H:%M")
+        rest_end_time = datetime.datetime.strptime(rest_time["end_time"], "%H:%M")
+        if dt_start_time <= rest_start_time and rest_end_time <= dt_end_time:
+            total_rest_time_minute += int((rest_end_time - rest_start_time).total_seconds() / 60)
+        elif dt_start_time <= rest_start_time and rest_end_time > dt_end_time:
+            total_rest_time_minute += int((rest_end_time - dt_end_time).total_seconds() / 60)
+        elif dt_start_time > rest_start_time and rest_end_time <= dt_end_time:
+            total_rest_time_minute += int((dt_start_time - rest_start_time).total_seconds() / 60)
+
+    work_time_minute = int(diff_time.total_seconds() / 60) - total_rest_time_minute
+
+    return work_time_minute, total_rest_time_minute
+
+
+if __name__ == "__main__":
+    pass
