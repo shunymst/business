62496a15f4452ada86c3321645d6d1a6bac868a0
miyamoto.atsushi <miyamoto.atsushi@tis.co.jp>
2017-07-06 15:40:13 +0900

    e

--- attendance/users.py
+++ attendance/users.py
@@ -4,7 +4,7 @@ from util import db_connection
 
 
 def get(db_conn, request_json):
-    sql = "select id, email, name, employee_division, employee_division_name from users where id = (%s) and enabled = true"
+    sql = "select id, email, name, employee_division, (select code_name from code_master where class='0001' and code=users.employee_division) as employee_division_name from users where id = (%s) and enabled = true"
     param = [request_json["mid"]]
     user_info = db_conn.select_dict(sql, param)
 
