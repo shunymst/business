807e602755babe4a224991de0ec8712205adac70
miyamoto.atsushi <miyamoto.atsushi@tis.co.jp>
2017-07-10 10:19:16 +0900

    tets

--- attendance/time_master.py
+++ attendance/time_master.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import copy
 import datetime
 from util.common_module import print_stdout
 
@@ -14,100 +15,12 @@ def get_work_time(db_conn, request_json):
     return None
 
 
-def get_delay_and_early_flag(request_json, work_time):
-
-    dt_start_time = datetime.datetime.strptime(request_json["start_time"].split(" ")[1], "%H:%M:%S")
-    dt_end_time = datetime.datetime.strptime(request_json["end_time"].split(" ")[1], "%H:%M:%S")
-    work_start_time = datetime.datetime.strptime(str(work_time["start_time"]), "%H:%M:%S")
-    work_end_time = datetime.datetime.strptime(str(work_time["end_time"]), "%H:%M:%S")
-
-    print("IN:", str(dt_start_time), str(dt_end_time), str(work_start_time), str(work_end_time))
-
-    # 日またぎ業務の場合
-    if dt_start_time > dt_end_time:
-        dt_end_time += datetime.timedelta(days=1)
-
-    # 規定勤務時間が日またぎ業務の場合
-    if work_start_time > work_end_time:
-
-        # 規定勤務時間の終了時間を+1日
-        work_end_time += datetime.timedelta(days=1)
-
-        # 開始時間が規定勤務時間の終了時間より小さい場合は+1日
-        if dt_start_time < work_end_time:
-            dt_start_time += datetime.timedelta(days=1)
-
-        # 終了時間が規定勤務時間の終了時間より小さい場合は+1日
-        if dt_end_time < work_end_time:
-            dt_end_time += datetime.timedelta(days=1)
-
-    delay_flag = "0"
-    if (work_start_time < dt_start_time) and (dt_start_time < work_end_time):
-        delay_flag = "1"
-    early_flag = "0"
-    if (work_start_time < dt_end_time) and (dt_end_time < work_end_time):
-        early_flag = "1"
-
-    print("OUT:", str(dt_start_time), str(dt_end_time), str(work_start_time), str(work_end_time))
-
-    return delay_flag, early_flag
-
-
 def get_rest_time(db_conn, request_json):
     sql = "select start_time, end_time from time_master where employee_division = (%s) and time_division = '2' "
     param = [request_json["employee_division"]]
     results = db_conn.select_dict(sql, param)
 
-    return get_minute_work_rest_time(
-        request_json["start_time"],
-        request_json["end_time"],
-        results
-    )
-
-
-def get_minute_work_rest_time(str_start_time, str_end_time, rest_time_list):
-
-    # TODO: 形式変わりそう
-    dt_start_time = datetime.datetime.strptime(str_start_time.split(" ")[1], "%H:%M:%S")
-    dt_end_time = datetime.datetime.strptime(str_end_time.split(" ")[1], "%H:%M:%S")
-
-    # 日またぎ業務の場合
-    if dt_start_time >= dt_end_time:
-        dt_end_time += datetime.timedelta(days=1)
-
-    diff_time = dt_end_time - dt_start_time
-
-    total_rest_time_minute = 0
-    for i, rest_time in enumerate(rest_time_list):
-        rest_start_time = datetime.datetime.strptime(str(rest_time["start_time"]), "%H:%M:%S")
-        rest_end_time = datetime.datetime.strptime(str(rest_time["end_time"]), "%H:%M:%S")
-
-        # 作業開始時間より休憩時間の開始時間が先の場合は+1日
-        if rest_start_time < dt_start_time:
-            rest_start_time += datetime.timedelta(days=1)
-
-        # 作業開始時間より休憩時間の終了時間が先の場合は+1日
-        if rest_end_time < dt_start_time:
-            rest_end_time += datetime.timedelta(days=1)
-
-        print_stdout(str(rest_start_time))
-        print_stdout(str(rest_end_time))
-
-        # if dt_start_time <= rest_start_time <= rest_end_time <= dt_end_time:
-        if (dt_start_time <= rest_start_time) and (rest_end_time <= dt_end_time):
-            total_rest_time_minute += int((rest_end_time - rest_start_time).total_seconds() / 60)
-        # elif dt_start_time <= rest_start_time < dt_end_time:
-        elif (dt_start_time <= rest_start_time) and (rest_start_time < dt_end_time):
-            total_rest_time_minute += int((dt_end_time - rest_start_time).total_seconds() / 60)
-        # elif dt_start_time < rest_end_time <= dt_end_time:
-        elif (dt_start_time < rest_end_time) and (rest_end_time <= dt_end_time):
-            total_rest_time_minute += int((rest_end_time - dt_start_time).total_seconds() / 60)
-
-        print_stdout(str(total_rest_time_minute))
-
-    work_time_minute = int(diff_time.total_seconds() / 60) - total_rest_time_minute
-
-    return work_time_minute, total_rest_time_minute
+    return results
 
 
 if __name__ == "__main__":
--- dp_business_server.py
+++ dp_business_server.py
@@ -1,6 +1,8 @@
 import copy
 import json
 import os
+import datetime
+import re
 
 from flask import Flask, request, jsonify
 
@@ -234,27 +236,46 @@ def attendance_result_check_result():
     request_json = get_request_param(request)
 
     send_content = results.check_result(g_db_conn, request_json)
-    
+
     return create_result_json(send_content)
 
-# 勤務時間、休憩時間計算
+# 勤務時間、休憩時間、中断時間計算
 @app.route("/attendance/result/calc_time", methods=["GET", "POST"])
 def attendance_result_calc_time():
 
     request_json = get_request_param(request)
 
     # 作業時間・休憩時間取得
-    work_time_minute, total_rest_time_minute = time_master.get_rest_time(g_db_conn, request_json)
+    rest_time_list = time_master.get_rest_time(g_db_conn, request_json)
+
+    # 業務外作業時間・休憩時間取得
+    outside_work_time_list = convert_time_list(request_json["outside_work_time_list"])
+    interruption_time_list = convert_time_list(request_json["interruption_time_list"])
+
+    # 作業時間・休憩時間・中断時間計算
+    work_time_minute, total_rest_time_minute, total_outside_work_time_minute, total_interruption_time_minute = \
+        get_minute_work_rest_time(
+            request_json["start_time"],
+            request_json["end_time"],
+            rest_time_list,
+            outside_work_time_list,
+            interruption_time_list
+        )
+
     str_work_time = common_module.format_hour_minute(work_time_minute)
     str_rest_time = common_module.format_hour_minute(total_rest_time_minute)
+    str_outside_work_time = common_module.format_hour_minute(total_rest_time_minute)
+    str_interruption_time = common_module.format_hour_minute(total_rest_time_minute)
 
     # 遅刻判定取得
     work_time = time_master.get_work_time(g_db_conn, request_json)
-    delay_flag, early_flag = time_master.get_delay_and_early_flag(request_json, work_time)
+    delay_flag, early_flag = get_delay_and_early_flag(request_json, work_time)
 
     send_content = {
         "work_time": str_work_time,
         "rest_time": str_rest_time,
+        "outside_work_time": str_outside_work_time,
+        "interruption_time": str_interruption_time,
         "delay_flag": delay_flag,
         "early_flag": early_flag,
         "message": "OK"
@@ -383,6 +404,161 @@ def create_result_json(send_content):
         json.dumps(send_content, indent=4, separators=(",", ": "), ensure_ascii=False)
     ))
     return jsonify(send_content)
+
+
+def get_delay_and_early_flag(request_json, work_time):
+
+    # 作業開始・終了時間 日付変換
+    dt_start_time = datetime.datetime.strptime(request_json["start_time"].split(" ")[1], "%H:%M:%S")
+    dt_end_time = datetime.datetime.strptime(request_json["end_time"].split(" ")[1], "%H:%M:%S")
+    work_start_time = datetime.datetime.strptime(str(work_time["start_time"]), "%H:%M:%S")
+    work_end_time = datetime.datetime.strptime(str(work_time["end_time"]), "%H:%M:%S")
+
+    print("IN:", str(dt_start_time), str(dt_end_time), str(work_start_time), str(work_end_time))
+
+    # 日またぎ業務の場合
+    if dt_start_time > dt_end_time:
+        dt_end_time += datetime.timedelta(days=1)
+
+    # 規定勤務時間が日またぎ業務の場合
+    if work_start_time > work_end_time:
+
+        # 規定勤務時間の終了時間を+1日
+        work_end_time += datetime.timedelta(days=1)
+
+        # 開始時間が規定勤務時間の終了時間より小さい場合は+1日
+        if dt_start_time < work_end_time:
+            dt_start_time += datetime.timedelta(days=1)
+
+        # 終了時間が規定勤務時間の終了時間より小さい場合は+1日
+        if dt_end_time < work_end_time:
+            dt_end_time += datetime.timedelta(days=1)
+
+    delay_flag = "0"
+    if (work_start_time < dt_start_time) and (dt_start_time < work_end_time):
+        delay_flag = "1"
+    early_flag = "0"
+    if (work_start_time < dt_end_time) and (dt_end_time < work_end_time):
+        early_flag = "1"
+
+    print("OUT:", str(dt_start_time), str(dt_end_time), str(work_start_time), str(work_end_time))
+
+    return delay_flag, early_flag
+
+
+def get_minute_work_rest_time(str_start_time, str_end_time, rest_time_list, outside_work_time_list,
+                              interruption_time_list):
+
+    # TODO: 形式変わる
+    dt_start_time = datetime.datetime.strptime(str_start_time.split(" ")[1], "%H:%M:%S")
+    dt_end_time = datetime.datetime.strptime(str_end_time.split(" ")[1], "%H:%M:%S")
+
+    # 日またぎ業務の場合
+    if dt_start_time > dt_end_time:
+        dt_end_time += datetime.timedelta(days=1)
+
+    print_stdout(str(str_start_time))
+    print_stdout(str(str_end_time))
+
+    work_time_list = [create_time_json(dt_start_time, dt_end_time)]
+    # 休憩時間計算
+    total_rest_time_minute = calc_work_time_and_interruption_time(dt_start_time, work_time_list, rest_time_list)
+
+    # 業務外作業時間計算(休憩時間とかぶる場合は休憩時間が優先される)
+    total_outside_work_time_minute = calc_work_time_and_interruption_time(dt_start_time, work_time_list, outside_work_time_list)  # noqa
+
+    # 中断時間計算(休憩時間とかぶる場合は休憩時間が優先される)
+    total_interruption_time_minute = calc_work_time_and_interruption_time(dt_start_time, work_time_list, interruption_time_list)  # noqa
+
+    diff_time = dt_end_time - dt_start_time
+    work_time_minute = \
+        int(diff_time.total_seconds() / 60) - total_outside_work_time_minute - total_interruption_time_minute
+
+    return work_time_minute, total_rest_time_minute, total_outside_work_time_minute, total_interruption_time_minute
+
+
+def calc_work_time_and_interruption_time(dt_start_time, work_time_list, interruption_time_list):
+    interruption_time_minute = 0
+    for interruption_time in interruption_time_list:
+        interruption_start_time = datetime.datetime.strptime(str(interruption_time["start_time"]), "%H:%M:%S")
+        interruption_end_time = datetime.datetime.strptime(str(interruption_time["end_time"]), "%H:%M:%S")
+
+        # 日またぎ休憩時間の場合
+        if interruption_start_time > interruption_end_time:
+            interruption_end_time += datetime.timedelta(days=1)
+
+        # 作業開始時間より中断時間の終了時間が過去の場合は中断時間を+1日で考える
+        if interruption_end_time < dt_start_time:
+            interruption_start_time += datetime.timedelta(days=1)
+            interruption_end_time += datetime.timedelta(days=1)
+
+        print_stdout(str(interruption_start_time))
+        print_stdout(str(interruption_end_time))
+
+        work_time_list_roop = copy.deepcopy(work_time_list)
+        for work_time in work_time_list_roop:
+            if (work_time["start_time"] <= interruption_start_time) \
+                    and (interruption_end_time <= work_time["end_time"]):
+                work_time_list.remove(work_time)
+
+                if work_time["start_time"] < interruption_start_time:
+                    work_time_list.append(
+                        create_time_json(work_time["start_time"], interruption_start_time)
+                    )
+                if interruption_end_time < work_time["end_time"]:
+                    work_time_list.append(
+                        create_time_json(interruption_end_time, work_time["end_time"])
+                    )
+                interruption_time_minute += int((interruption_end_time - interruption_start_time).total_seconds() / 60)
+                print("1:{}".format(work_time_list))
+                break
+            elif (work_time["start_time"] <= interruption_start_time) \
+                    and (interruption_start_time < work_time["end_time"]):
+                work_time_list.remove(work_time)
+                if work_time["start_time"] < interruption_start_time:
+                    work_time_list.append(
+                        create_time_json(work_time["start_time"], interruption_start_time)
+                    )
+                interruption_time_minute += int((work_time["end_time"] - interruption_start_time).total_seconds() / 60)
+                print("2:{}".format(work_time_list))
+            elif (work_time["start_time"] < interruption_end_time) and (interruption_end_time <= work_time["end_time"]):
+                work_time_list.remove(work_time)
+                if interruption_end_time < work_time["end_time"]:
+                    work_time_list.append(
+                        create_time_json(interruption_end_time, work_time["end_time"])
+                    )
+                interruption_time_minute += int((interruption_end_time - work_time["start_time"]).total_seconds() / 60)
+                print("3:{}".format(work_time_list))
+            elif (interruption_start_time <= work_time["start_time"]) \
+                    and (work_time["end_time"] <= interruption_end_time):
+                work_time_list.remove(work_time)
+                interruption_time_minute += int((work_time["end_time"] - work_time["start_time"]).total_seconds() / 60)
+                print("4:{}".format(work_time_list))
+            else:
+                print("5:{}".format(work_time_list))
+
+    return interruption_time_minute
+
+
+def create_time_json(start_time, end_time):
+    return {"start_time": start_time, "end_time": end_time}
+
+
+def convert_time_list(str_timelist):
+    dict_time_list = []
+    if str_timelist:
+        list_time = str_timelist.split(";")
+        for time_part in list_time:
+            if time_part:
+                dict_time_list.append(
+                    {
+                        "start_time": time_part[0: time_part.index("～")].split(" ")[1],
+                        "end_time": time_part[time_part.index("～") + 1: time_part.index("(")].split(" ")[1],
+                        "remarks": time_part[time_part.index("(") + 1: len(time_part) - 1]
+                    }
+                )
+
+    return dict_time_list
 
 
 if __name__ == "__main__":
