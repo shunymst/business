fa1679fd1802dca9b57fe43006881ea11197db73
miyamoto.atsushi <miyamoto.atsushi@tis.co.jp>
2017-07-10 14:24:08 +0900

    aaa

--- attendance/interruption.py
+++ attendance/interruption.py
@@ -22,20 +22,20 @@ def insert(db_conn, request_json):
         ]
         db_conn.update(sql, param)
 
-    # 業務外作業時間登録
-    outside_work_time_list = attendans_util.convert_time_list(request_json["outside_work_time_list"])
-    interruption_division = "2"
-    for i, outside_work_time in enumerate(outside_work_time_list):
-        param = [
-            request_json["user_id"],
-            request_json["attendance_date"],
-            interruption_division,
-            i + 1,
-            outside_work_time["start_time"],
-            outside_work_time["end_time"],
-            outside_work_time["reason"]
-        ]
-        db_conn.update(sql, param)
+    # # 業務外作業時間登録
+    # outside_work_time_list = attendans_util.convert_time_list(request_json["outside_work_time_list"])
+    # interruption_division = "2"
+    # for i, outside_work_time in enumerate(outside_work_time_list):
+    #     param = [
+    #         request_json["user_id"],
+    #         request_json["attendance_date"],
+    #         interruption_division,
+    #         i + 1,
+    #         outside_work_time["start_time"],
+    #         outside_work_time["end_time"],
+    #         outside_work_time["reason"]
+    #     ]
+    #     db_conn.update(sql, param)
 
     return "OK"
 
--- routing/attendance.py
+++ routing/attendance.py
@@ -39,7 +39,7 @@ def attendance_result_init():
 
     send_content = {
         "interruption_time": "なし",
-        "outside_work_time": "なし"
+        # "outside_work_time": "なし"
     }
 
     return routing_util.create_result_json(send_content)
@@ -67,7 +67,7 @@ def attendance_result_calc_time():
 
     # 中断時間・業務外勤務時間設定
     interruption_time_list = attendans_util.convert_time_list(request_json["interruption_time_list"])
-    outside_work_time_list = attendans_util.convert_time_list(request_json["outside_work_time_list"])
+    # outside_work_time_list = attendans_util.convert_time_list(request_json["outside_work_time_list"])
 
     # 作業時間・休憩時間・中断時間計算
     work_time_minute, total_rest_time_minute, total_interruption_time_minute, total_outside_work_time_minute = \
@@ -76,13 +76,13 @@ def attendance_result_calc_time():
             request_json["end_time"],
             rest_time_list,
             interruption_time_list,
-            outside_work_time_list
+            # outside_work_time_list
         )
 
     str_work_time = attendans_util.format_hour_minute(work_time_minute)
     str_rest_time = attendans_util.format_hour_minute(total_rest_time_minute)
     str_interruption_time = attendans_util.format_hour_minute(total_interruption_time_minute)
-    str_outside_work_time = attendans_util.format_hour_minute(total_outside_work_time_minute)
+    # str_outside_work_time = attendans_util.format_hour_minute(total_outside_work_time_minute)
 
     # 遅刻判定取得
     work_time = time_master.get_work_time(g_db_conn, request_json)
@@ -92,7 +92,7 @@ def attendance_result_calc_time():
         "work_time": str_work_time,
         "rest_time": str_rest_time,
         "interruption_time": str_interruption_time,
-        "outside_work_time": str_outside_work_time,
+        # "outside_work_time": str_outside_work_time,
         "delay_early_flag": delay_flag or early_flag,
         "message": "OK"
     }
