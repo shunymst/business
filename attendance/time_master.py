9194857c9227a8cc448917de7666278eb7711125
miyamoto.atsushi <miyamoto.atsushi@tis.co.jp>
2017-07-10 14:00:38 +0900

    aa

--- attendance/time_master.py
+++ attendance/time_master.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from attendance import code_master
 
 
 def get_work_time(db_conn, request_json):
-    sql = "select start_time, end_time from time_master where employee_division = (%s) and time_division = '1' "
-    param = [request_json["employee_division"]]
+
+    # 勤務区分コード変換
+    work_division = code_master.change_code_by_name(
+        db_conn, code_master.CLASS_WORK_DIVISION,
+        request_json["work_division"],
+        request_json["employee_division"]
+    )
+
+    sql = "select start_time, end_time from time_master where id = (select division2 from code_master where code = (%s)) and time_division = '1' "  # noqa
+    param = [work_division]
     results = db_conn.select_dict(sql, param)
 
     if results and len(results) > 0:
@@ -13,8 +22,16 @@ def get_work_time(db_conn, request_json):
 
 
 def get_rest_time(db_conn, request_json):
-    sql = "select start_time, end_time from time_master where employee_division = (%s) and time_division = '2' "
-    param = [request_json["employee_division"]]
+
+    # 勤務区分コード変換
+    work_division = code_master.change_code_by_name(
+        db_conn, code_master.CLASS_WORK_DIVISION,
+        request_json["work_division"],
+        request_json["employee_division"]
+    )
+
+    sql = "select start_time, end_time from time_master where id = (select division2 from code_master where code = (%s)) and time_division = '2' "  # noqa
+    param = [work_division]
     results = db_conn.select_dict(sql, param)
 
     return results
