6e195fb04be62ba3661ddf9fdc415654c7aa6c64
miyamoto.atsushi <miyamoto.atsushi@tis.co.jp>
2017-07-14 17:04:47 +0900

    aa

--- attendance/users.py
+++ attendance/users.py
@@ -5,9 +5,9 @@
 def get(db_conn, request_json):
     sql = "select id, email, name, employee_division, " \
           "(select code_name from code_master where class='0001' and code=users.employee_division) as employee_division_name " \
-          "from users where mid = (%s) and enabled = true"  # noqa
+          "from users where mid = %(mid)s and enabled = true"  # noqa
     param = {
-        request_json["mid"]
+        "mid": request_json["mid"]
     }
     user_info = db_conn.select_dict(sql, param)
 
