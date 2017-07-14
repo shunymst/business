2201c57dc4631de0551922472b325fb0340ebe1b
miyamoto.atsushi <miyamoto.atsushi@tis.co.jp>
2017-07-14 15:08:46 +0900

    aa

--- attendance/users.py
+++ attendance/users.py
@@ -3,8 +3,22 @@
 
 
 def get(db_conn, request_json):
-    sql = "select id, email, name, employee_division, (select code_name from code_master where class='0001' and code=users.employee_division) as employee_division_name from users where mid = (%s) and enabled = true"  # noqa
+    sql = "select id, email, name, employee_division, " \
+          "(select code_name from code_master where class='0001' and code=users.employee_division) as employee_division_name " \
+          "from users where mid = (%s) and enabled = true"  # noqa
     param = [request_json["mid"]]
     user_info = db_conn.select_dict(sql, param)
 
     return user_info
+
+
+def get_detail(db_conn, request_json):
+    sql = "select id, email, name, employee_division, " \
+          "(select code_name from code_master where class='0001' and code=users.employee_division) as employee_division_name " \
+          "from users where mid = (%s) and enabled = true"  # noqa
+    param = [request_json["mid"]]
+    user_info = db_conn.select_dict(sql, param)
+
+    return user_info
+
+
