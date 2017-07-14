200ef4bcfe1bcc68ea2bb5461bc390576902414a
miyamoto.atsushi <miyamoto.atsushi@tis.co.jp>
2017-07-14 09:57:58 +0900

    aaa

--- db_test.py
+++ db_test.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     g_ini_def = common_module.read_ini("conf/environment.ini")["DEFAULT"]
     # DB Connection作成
     g_db_conn = db_connection.DBConn(g_ini_def["DB_CONNECTION_STR"])
-    resuls = g_db_conn.select_dict("select %(test)s, %(test2)s, %(test)s", {"test": "a", "test2": "b", })
+    resuls = g_db_conn.select_dict("select %(test)s as a, %(test2)s as b, %(test)s as c", {"test": "a", "test2": "b", })
 
     print(json.dumps(resuls, indent=4, separators=(",", ": "), ensure_ascii=False))
